6992801a585c1e3e2d8d6b930014bccc511ac4f5
JacquesLassau <37740292+JacquesLassau@users.noreply.github.com>
2020-06-17 01:20:33 -0300

    Procedure  de acesso do Usuário
    Procedure de acesso ao usuário do sistema (Atendente)

---
+++ Stored Procedures/Usuario/SP_AcessoUsuarioV1.sql
@@ -0,0 +1,31 @@
+USE DB_LOKANDO
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Jacques de Lassau>
+-- Create date: <14/06/2020>
+-- Description:	<Verificar acesso de Usuario por e-mail e senha>
+-- ============================================= 
+CREATE PROCEDURE [dbo].[SP_AcessoUsuarioV1]
+	@USEMAILLOK varchar(100),
+	@USSENHALOK varchar(100)
+AS
+BEGIN
+	BEGIN TRAN 
+	IF NOT EXISTS (Select USEMAILLOK,USSENHALOK From DB_LOKANDO..TBUSULOK With(nolock) Where USEMAILLOK = @USEMAILLOK And USSENHALOK = @USSENHALOK And USTPUSULOK = 'A' And USSITLOK = 'A')
+	BEGIN			
+		PRINT 'E-mail ou senha inválido.'
+		ROLLBACK
+	END	
+	ELSE
+	BEGIN
+		Select USEMAILLOK,USSENHALOK From DB_LOKANDO..TBUSULOK With(nolock) Where USEMAILLOK = @USEMAILLOK And USSENHALOK = @USSENHALOK And USTPUSULOK = 'A' And USSITLOK = 'A'
+		COMMIT
+	END
+END
+GO
